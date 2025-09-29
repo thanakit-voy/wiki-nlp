185d66e6da7cc0e93f0d0a735246eb14cfe06a7c
thanakit-voy <thanakit.voy@gmail.com>
2025-09-29 09:55:35 +0700

    Enhance run.ps1: add GPU, MongoDB connection, and environment variable support

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -1,13 +1,19 @@
 param(
     [string]$Tag = "wiki-nlp-cli",
     [string]$Name = "world",
-    [switch]$Upper
+    [switch]$Upper,
+    [switch]$Gpu,
+    [string]$MongoUri = "mongodb://host.docker.internal:27017",
+    [string]$MongoDb = ""
 )
 
-$dockerArgs = @("run", "--rm", $Tag, "--name", $Name)
-if ($Upper) {
-    $dockerArgs += "--upper"
-}
+$dockerArgs = @("run", "--rm")
+if ($Gpu) { $dockerArgs += @("--gpus", "all") }
+if ($MongoUri -ne "") { $dockerArgs += @("-e", "MONGO_URI=$MongoUri") }
+if ($MongoDb -ne "") { $dockerArgs += @("-e", "MONGO_DB=$MongoDb") }
+
+$dockerArgs += @($Tag, "--name", $Name)
+if ($Upper) { $dockerArgs += "--upper" }
 
 Write-Host "Running: docker $($dockerArgs -join ' ')"
 docker @dockerArgs
